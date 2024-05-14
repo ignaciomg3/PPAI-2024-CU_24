a22e30ede4f92608b1121b83d46482338b3a87da
Ignacio Mongi <ignaciomg3@gmail.com>
2024-05-14 18:24:50 -0300

    2do Commit - Hago el login de inicio

--- CU-24_GenerarReporte/Login.cs
+++ CU-24_GenerarReporte/Login.cs
@@ -16,5 +16,10 @@ namespace CU_24_GenerarReporte
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
