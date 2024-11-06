df3c1063415376994732cf045e194620ee4d4375
Ignacio Andres Mongi <ignaciomg3@gmail.com>
2024-11-06 02:39:28 -0300

    Creo Carpeta Datos y Clase GestorDB

---
+++ PPAI-2024-CU_24/CU-24_GenerarReporte/Datos/GestorBD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CU_24_GenerarReporte.Datos
+{
+    public class GestorBD
+    {
+        private string cadenaConexion { get; set; }
+
+        public GestorBD(string cadenaConexion)
+        {
+            this.cadenaConexion = cadenaConexion;
+        }
+
+
+
+    }
+}
